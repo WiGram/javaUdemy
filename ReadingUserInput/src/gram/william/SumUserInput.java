8e19340d0b9c20815c1a00c6dd4267733a253183
WiGram <william.gram@outlook.com>
2020-01-09 23:01:40 +0100

    beautify challenge code

--- ReadingUserInput/src/gram/william/SumUserInput.java
+++ ReadingUserInput/src/gram/william/SumUserInput.java
@@ -8,18 +8,20 @@ public class SumUserInput {
         int inputs = 1;
         int inputSum = 0;
         while (inputs < 11) {
+
             System.out.println("Enter integer no. " + inputs + ": ");
             boolean inputIsInt = scanner.hasNextInt();
+
             if (!inputIsInt) {
                 System.out.println("Value is not an int, type in int: ");
-                continue;
             } else {
                 int userInt = scanner.nextInt();
-                scanner.nextLine();
                 inputSum += userInt;
-                inputs += 1;
-                System.out.println("Preliminary sum is: " + inputSum);
+                inputs++;
             }
+            System.out.println("Preliminary sum is: " + inputSum);
+
+            scanner.nextLine();
         }
         System.out.println("Final sum is: " + inputSum);
         scanner.close();
