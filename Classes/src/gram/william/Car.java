2bf6e97f7020f697b532ca54e537114c48817ed3
WiGram <william.gram@outlook.com>
2020-01-10 07:43:29 +0100

    First lecture on classes

---
+++ Classes/src/gram/william/Car.java
@@ -0,0 +1,20 @@
+package gram.william;
+
+// access modifier:
+// public - unrestricted access
+// protected - other classes within the package can access the class
+// private - no other class can get access
+
+public class Car {
+    // encapsulation is the concept of hiding objects outside the object's definition
+
+    // Define the state of the car, defined as fields
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+    // Five fields that define the state of the car above.
+
+
+}
---
+++ Classes/src/gram/william/Main.java
@@ -0,0 +1,17 @@
+package gram.william;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // As far as Java is concerned, Car (a class) works as a new data type
+        Car porsche = new Car();
+        Car holden = new Car();  // an Australian brand car
+
+        // Many methods are inherited from a base Java class, such as the method equals
+        // porsche.equals();
+
+        // porsche.model = "Carrera";  Against encapsulation - better make it private and add a method.
+        
+    }
+}
