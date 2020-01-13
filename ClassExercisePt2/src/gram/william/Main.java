91ad9083cd32184cbb319134b20e5c90d24fe706
WiGram <william.gram@outlook.com>
2020-01-13 23:01:20 +0100

    exercises

---
+++ ClassExercisePt2/src/gram/william/Main.java
@@ -0,0 +1,9 @@
+package gram.william;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Wall wall = new Wall(1.125, -1.0);
+        System.out.println(wall.getHeight());
+    }
+}
