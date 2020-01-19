068545d1826df8a9535c60a3c6b8d170c0d7420a
WiGram <william.gram@outlook.com>
2020-01-19 10:53:59 +0100

    sorting algorithm

---
+++ ArrayChallenge/src/gram/william/Main.java
@@ -0,0 +1,55 @@
+package gram.william;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] testArray = getIntegers(5);
+        printArray(testArray);
+        int[] testSort = sortIntegers(testArray);
+        printArray(testSort);
+    }
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int[] getIntegers(int arrayLength) {
+        System.out.println("Enter " + arrayLength + " integers to go into your array.");
+
+        int[] myArray = new int[arrayLength];
+
+        for (int i=0; i<arrayLength; i++) {
+            myArray[i] = scanner.nextInt();
+        }
+
+        return myArray;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i=0; i<array.length; i++) {
+            System.out.println("Array index " + i + " has value " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+
+        int oldArrayNumber = 0;
+        int oldMax = 0;
+        int tmpLength = array.length;
+        int[] sortedArray = new int[array.length];
+        Arrays.fill(sortedArray, Integer.MIN_VALUE);
+
+        for (int i = 0; i<array.length; i++) {
+            oldArrayNumber = array[i];
+            for (int j = tmpLength-1; j >= 0; j--) {
+                if (oldArrayNumber > sortedArray[j]) {
+                    oldMax = sortedArray[j];
+                    sortedArray[j] = oldArrayNumber;
+                    oldArrayNumber = oldMax;
+                }
+            }
+        }
+        return sortedArray;
+    }
+}
